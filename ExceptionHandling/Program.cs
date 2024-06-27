68d9c80d1be5bb77ad21d3d55b2a8e9f781cb5fa
Youfang Yao <yyfsingler@163.com>
2024-06-26 19:55:03 -0700

    add new demo project for Exception Handling

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string[] cars = new string[3];
+                cars[0] = "BMW";
+                cars[1] = "Mazda";
+                cars[2] = "Honda";
+
+                for (int i = 0; i <= 5; i++)
+                {
+                    Console.WriteLine(cars[i]);
+                }
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Only 3 cars are allowed each time!");
+                //Console.WriteLine(ex.ToString());
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("End!");
+            }
+        }
+    }
+}
